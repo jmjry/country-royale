54da1bcf140192f615e680399053a34401ec030c
jmjry <jakeryan1998@icloud.com>
2021-09-08 13:07:40 +0000

    Removed event listener

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-	let buttons = document.getElementsByTagName("button");
-
-	for (let button of buttons) {
-		button.addEventListener("click", function() {
-			if (this.getAttribute("data-type") === "submit") {
-				checkAnswer();
-			} else {
-				let gameType = this.getAttribute("data-type");
-				runGame(gameType);
-			}
-		});
-	}
-
-	document.getElementById("answer-box").addEventListener("keydown", function(event) {
-		if (event.key === "Enter") {
-			checkAnswer();
-		}
-	});
-
-	runGame("game");
-});
-
 /* List of countries & their land mass */
 
 const countries = [
