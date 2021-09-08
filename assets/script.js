1768181727536544362fad8fa0bc1b275744dca6
jmjry <jakeryan1998@icloud.com>
2021-09-08 13:08:15 +0000

    Reorganised countries array

--- assets/script.js
+++ assets/script.js
@@ -1,66 +1,21 @@
 /* List of countries & their land mass */
 
 const countries = [
-    {
-      'name': 'Spain',
-      'size_km2': '505990',
-    },
-    {
-      'name': 'Egypt',
-      'size_km2': '1010000',
-    },
-    {
-        'name': 'England',
-        'size_km2': '1010000',
-      },
-      {
-        'name': 'Canada',
-        'size_km2': '9985000',
-      },
-      {
-        'name': 'Yemen',
-        'size_km2': '555000',
-      },
-      {
-        'name': 'Turkey',
-        'size_km2': '783562',
-      },
-      {
-        'name': 'Brazil',
-        'size_km2': '8516000',
-      },
-      {
-        'name': 'France',
-        'size_km2': '632734',
-      },
-      {
-        'name': 'Peru',
-        'size_km2': '1285000',
-      },
-      {
-        'name': 'Mexico',
-        'size_km2': '1973000',
-      },
-      {
-        'name': 'Germany',
-        'size_km2': '357386',
-      },
-      {
-        'name': 'Poland',
-        'size_km2': '312679',
-      },
-      {
-        'name': 'Greece',
-        'size_km2': '131957',
-      },
-      {
-        'name': 'Ireland',
-        'size_km2': '84421',
-      },
-      {
-        'name': 'Kazakhstan',
-        'size_km2': '2725000',
-      },
+  {'name': 'Spain', 'size_km2': 505990},
+  {'name': 'Egypt','size_km2': 1010000},
+  {'name': 'England','size_km2': 1010000},
+  {'name': 'Canada','size_km2': 9985000},
+  {'name': 'Yemen','size_km2': 555000},
+  {'name': 'Turkey','size_km2': 783562},
+  {'name': 'Brazil','size_km2': 8516000},
+  {'name': 'France','size_km2': 632734},
+  {'name': 'Peru','size_km2': 1285000},
+  {'name': 'Mexico','size_km2': 1973000},
+  {'name': 'Germany','size_km2': 357386},
+  {'name': 'Poland','size_km2': 312679},
+  {'name': 'Greece', 'size_km2': 131957},
+  {'name': 'Ireland', 'size_km2': 84421},
+  {'name': 'Kazakhstan','size_km2': 2725000,},
 
   ]
 
