63e65ecd0817078172123f0254820e2aac2d565b
jmjry <jakeryan1998@icloud.com>
2021-09-29 14:05:46 +0000

    Added correct semicolons

--- assets/script.js
+++ assets/script.js
@@ -99,8 +99,8 @@ const countries = [{
 	'size_km2': 329847,
 	'image': ['assets/images/malaysia.png']
 },
-]
-const num_questions = 10
+];
+const num_questions = 10;
 
 /**  
  * Game logic setup 
@@ -109,42 +109,42 @@ const num_questions = 10
 */
 
 function setupGame() {
-let firstCountry = chooseRandomCountry()
-let secondCountry = chooseRandomCountry(firstCountry)
+let firstCountry = chooseRandomCountry();
+let secondCountry = chooseRandomCountry(firstCountry);
 
-document.getElementById('first_question_choice').checked = false
-document.getElementById('second_question_choice').checked = false
+document.getElementById('first_question_choice').checked = false;
+document.getElementById('second_question_choice').checked = false;
 
-document.getElementById('first_question').innerHTML = firstCountry.name
-document.getElementById('second_question').innerHTML = secondCountry.name
+document.getElementById('first_question').innerHTML = firstCountry.name;
+document.getElementById('second_question').innerHTML = secondCountry.name;
 
-document.getElementById('first_question_choice').value = firstCountry.name
-document.getElementById('second_question_choice').value = secondCountry.name
+document.getElementById('first_question_choice').value = firstCountry.name;
+document.getElementById('second_question_choice').value = secondCountry.name;
 
 /**  
  * Returns image associated with country 
  * @param img1 - gets an image from the array 
  * @param img2 - gets an image from the array */
 
-const img1 = document.querySelector("#first_question_image")
-img1.src = firstCountry.image
+const img1 = document.querySelector("#first_question_image");
+img1.src = firstCountry.image;
 
-const img2 = document.querySelector("#second_question_image")
-img2.src = secondCountry.image
+const img2 = document.querySelector("#second_question_image");
+img2.src = secondCountry.image;
 
 /**
  * @param total_score - calculates the total score
  */
 
-total_score = parseInt(document.getElementById('score').innerHTML) + parseInt(document.getElementById('incorrect').innerHTML)
+total_score = parseInt(document.getElementById('score').innerHTML) + parseInt(document.getElementById('incorrect').innerHTML);
 
 /**
  * @param - displays play_again if total score equals total number of questions
  */
 
 if (total_score >= num_questions) {
-	document.getElementById('game_area').style.display = "none"
-	document.getElementById('play_again').style.display = "block"
+	document.getElementById('game_area').style.display = "none";
+	document.getElementById('play_again').style.display = "block";
 }
 }
 
@@ -152,11 +152,11 @@ if (total_score >= num_questions) {
  * @param playAgain -Lets user play the game again with a starting score of 0 */
 
 function playAgain() {
-document.getElementById('score').innerHTML = 0
-document.getElementById('incorrect').innerHTML = 0
-document.getElementById('game_area').style.display = "block"
-document.getElementById('play_again').style.display = "none"
-setupGame()
+document.getElementById('score').innerHTML = 0;
+document.getElementById('incorrect').innerHTML = 0;
+document.getElementById('game_area').style.display = "block";
+document.getElementById('play_again').style.display = "none";
+setupGame();
 }
 
 
@@ -168,9 +168,9 @@ if (exclude) {
 	let filtered_countries = countries.filter(function(value, index, arr) {
 		return index != countries.indexOf(exclude);
 	});
-	return filtered_countries[getRandomArbitrary(0, filtered_countries.length)]
+	return filtered_countries[getRandomArbitrary(0, filtered_countries.length)];
 } else {
-	return countries[getRandomArbitrary(0, countries.length)]
+	return countries[getRandomArbitrary(0, countries.length)];
 }
 }
 
@@ -182,24 +182,24 @@ function getCountryByName(country_name) {
 let country = countries.filter(function(value, index, arr) {
 	return value.name == country_name;
 });
-return country[0]
+return country[0];
 }
 
 function getRandomArbitrary(min, max) {
-return Math.floor(Math.random() * (max - min) + min)
+return Math.floor(Math.random() * (max - min) + min);
 }
 
 
 /** 
  * @param setupGame - Sets up the game */
 
-setupGame()
+setupGame();
 
 function chooseAnswer() {
-chosen_answer = getCountryByName(getCountrySelected())
-form_data = getFormData()
-first_choice = getCountryByName(form_data[0])
-second_choice = getCountryByName(form_data[1])
+chosen_answer = getCountryByName(getCountrySelected());
+form_data = getFormData();
+first_choice = getCountryByName(form_data[0]);
+second_choice = getCountryByName(form_data[1]);
 
 /** 
  *  Calculates if the chosen answer is correct or incorrect 
@@ -207,20 +207,20 @@ second_choice = getCountryByName(form_data[1])
 
 if (chosen_answer.name == first_choice.name) {
 	if (chosen_answer.size_km2 > second_choice.size_km2) {
-		plus_score()
+		plus_score();
 	} else {
-		minus_score()
+		minus_score();
 	}
 }
 
 if (chosen_answer.name == second_choice.name) {
 	if (chosen_answer.size_km2 > first_choice.size_km2) {
-		plus_score()
+		plus_score();
 	} else {
-		minus_score()
+		minus_score();
 	}
 }
-setupGame()
+setupGame();
 }
 
 /** 
@@ -229,12 +229,12 @@ setupGame()
 */
 
 function plus_score() {
-return document.getElementById('score').innerHTML = parseInt(document.getElementById('score').innerHTML) + 1
+return document.getElementById('score').innerHTML = parseInt(document.getElementById('score').innerHTML) + 1;
 }
 
 
 function minus_score() {
-return document.getElementById('incorrect').innerHTML = parseInt(document.getElementById('incorrect').innerHTML) + 1
+return document.getElementById('incorrect').innerHTML = parseInt(document.getElementById('incorrect').innerHTML) + 1;
 }
 
 function getCountrySelected() {
@@ -248,6 +248,6 @@ for (i = 0; i < ele.length; i++) {
 
 function getFormData() {
 first_choice = document.getElementById('first_question_choice').value,
-	second_choice = document.getElementById('second_question_choice').value
-return [first_choice, second_choice]
+	second_choice = document.getElementById('second_question_choice').value;
+return [first_choice, second_choice];
 }
